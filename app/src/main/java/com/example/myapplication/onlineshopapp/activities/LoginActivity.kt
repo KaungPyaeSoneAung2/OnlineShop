3ce8137ae22b2f845f13da1ba9c3379572ed098b
KaungPyaeSoneAung2 <kaungpyae.kpp16@gmail.com>
2022-08-01 21:44:59 +0630

    In progress

--- app/src/main/java/com/example/myapplication/onlineshopapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/onlineshopapp/activities/LoginActivity.kt
@@ -3,19 +3,109 @@ package com.example.myapplication.onlineshopapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.myapplication.onlineshopapp.R
 import com.example.myapplication.onlineshopapp.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private companion object{
+        private const val TAG= "LoginActivity"
+        private const val RC_GOOGLE_SIGN_IN=4926
+    }
+    private lateinit var auth:FirebaseAuth
+
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth= Firebase.auth
+
+
+
 
         binding.noAccountSignUp.setOnClickListener{
             Intent(this,SignUpActivity::class.java).also { startActivity(it) }
+            finish()
+        }
+
+        binding.buttonLogin.setOnClickListener {
+            Toast.makeText(this, "Coming soon", Toast.LENGTH_SHORT).show()
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .requestProfile()
+            .build()
+
+        val client = GoogleSignIn.getClient(this,gso)
+        binding.googleLogin.setOnClickListener{
+            val signInIntent= client.signInIntent
+            startActivityForResult(signInIntent,RC_GOOGLE_SIGN_IN)
         }
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        //this go to main activtiy
+        if(user==null){
+            Log.w(TAG,"User is null, not going to navigate")
+            return
+        }
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== RC_GOOGLE_SIGN_IN){
+            val task=GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle"+account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            }catch (e: ApiException){
+                Log.w(TAG, "Google sign in failed",e)
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken,null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this){ task->
+                if(task.isSuccessful){
+                    Log.d(TAG,"signInWithCredential:success")
+                    val user= auth.currentUser
+                    updateUI(user)
+                }
+                else{
+                    Log.w(TAG,"signInWithCredential:failure",task.exception)
+                    Toast.makeText(this,"Authentication failed",Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/example/myapplication/onlineshopapp/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/myapplication/onlineshopapp/activities/SignUpActivity.kt
@@ -3,9 +3,14 @@ package com.example.myapplication.onlineshopapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.example.myapplication.onlineshopapp.R
-import com.example.myapplication.onlineshopapp.databinding.ActivityLoginBinding
 import com.example.myapplication.onlineshopapp.databinding.ActivitySignUpBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
@@ -14,20 +19,89 @@ class SignUpActivity : AppCompatActivity() {
         binding=ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        setupActionBar()
+
         binding.havAccountLogin.setOnClickListener{
             Intent(this,LoginActivity::class.java).also { startActivity(it) }
+            finish()
+        }
+        binding.buttonSignUp.setOnClickListener {
+            registerUser()
         }
 
     }
 
     private fun setupActionBar(){
-        setSupportActionBar()
+        setSupportActionBar(binding.topToolBar)
         val actionBar=supportActionBar
         if(actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true)
-            actionBar.setHomeAsUpIndicator(R.drawable.back)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
 
         }
-        .setNavigationOnClickListener{onBackPressed()}
+        binding.topToolBar.setNavigationOnClickListener{
+            Intent(this,LoginActivity::class.java).also { startActivity(it) }
+            finish()
+        }
+    }
+
+    private fun validateRegisterDetails(): Boolean{
+        return when{
+            TextUtils.isEmpty(binding.firstNameSignUp.text.toString().trim { it<= ' '})->{
+                (Toast.makeText(this, "First Name Error", Toast.LENGTH_SHORT).show())
+                false
+            }
+            TextUtils.isEmpty(binding.lastNameSignUp.text.toString().trim { it<= ' '})->{
+                (Toast.makeText(this, "Last Name Error", Toast.LENGTH_SHORT).show())
+                false
+            }
+            TextUtils.isEmpty(binding.emailSignUp.text.toString().trim { it<= ' '})|| !binding.emailSignUp.text.contains("mail.com")->{
+                (Toast.makeText(this, "Email Error", Toast.LENGTH_SHORT).show())
+                false
+            }
+            TextUtils.isEmpty(binding.pwSignUp.text.toString().trim { it<= ' '})->{
+                (Toast.makeText(this, "Password Error", Toast.LENGTH_SHORT).show())
+                false
+            }
+            TextUtils.isEmpty(binding.pwSignUp2.text.toString().trim { it<= ' '})->{
+                (Toast.makeText(this, "Password Error", Toast.LENGTH_SHORT).show())
+                false
+            }
+            binding.pwSignUp.text.toString() != binding.pwSignUp2.text.toString() ->{
+                (Toast.makeText(this, "Password not match", Toast.LENGTH_SHORT).show())
+                false
+            }
+            !binding.tandcSignUp.isChecked ->{
+                Toast.makeText(this, "If you don't agree our policy Just Goo", Toast.LENGTH_SHORT).show()
+                false
+            }
+            else ->{
+                true
+            }
+        }
+    }
+
+    private fun registerUser() {
+        if (validateRegisterDetails()) {
+            val email: String = binding.emailSignUp.text.toString().trim { it <= ' '}
+            val pw: String = binding.pwSignUp.text.toString().trim { it <= ' '}
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,pw)
+                .addOnCompleteListener(
+                    OnCompleteListener<AuthResult> { task ->
+                        if(task.isSuccessful){
+                         val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                            Toast.makeText(this, "Welcome. Your Id is ${firebaseUser.uid}", Toast.LENGTH_SHORT).show()
+                        }
+                        else{
+                            Toast.makeText(this, task.exception!!.message.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                )
+
+        }
+
+
     }
 }
